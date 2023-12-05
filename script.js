bd9437d632f56266a391cb52863aaa3f325803f1
Spencer Cox <spencox@gmail.com>
2023-12-05 01:11:44 -0600

    finished program, tested functionality, added some minor css

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
+// initialize array to hold recipes
 const recipes = [];
 
-// Update a recipe when the "Add Recipe" button is clicked
-// If the recipe is new, then add the recipe to the recipes array 
-// Use addRecipes() function to add the new recipe
-// Else edit the recipe in the recipes array
-// Clear the form's input fields using the clearInputFields() function
-// Finally, display the recipes using the displayRecipes() function
-
-
+// select add button and set up event listener
 document.getElementById('add-recipe-btn').addEventListener('click', function() {
-    console.log("Button Clicked")
+
+    // DOM Selectors for inputted values
     const title = document.getElementById('title').value;
     const ingredients = document.getElementById('ingredients').value;
     const instructions = document.getElementById('instructions').value;
     
+    // check if edit flag present 
     const index = this.getAttribute('data-index');
-    console.log(index);
     
     if (index !== null) {
         // Update an existing recipe
@@ -30,37 +25,55 @@ document.getElementById('add-recipe-btn').addEventListener('click', function() {
             clearInputFields();
         }
     }
-
     displayRecipes();
 });
 
-// TODO: Clear the form's input fields
+// clear the form's input fields
 function clearInputFields() {
-    // Write your code here for task 1
     document.getElementById('title').value = '';
     document.getElementById('ingredients').value = '';
     document.getElementById('instructions').value = '';
 }
 
-// TODO:Add the new recipe to the recipes array
+// add the new recipe to the recipes array
 function addRecipe(recipe) {
-    // Write your code here for task 2
     recipes.push(recipe);
-    displayRecipes(recipes);
 }
 
-// TODO: Display Recipes
+// display Recipes
 function displayRecipes() {
-    // Write your code here for task 3
-    console.log("Display Recipies");
-       
+    // DOM selector for ul of recopies
+    const recipesListEl = document.getElementById('recipes');
+    // clear list for new display of current recipes list
+    recipesListEl.innerHTML = '';
+    // iterate over recipes and create list items
+    recipes.forEach((recipe, index) => {
+        const recipesListItem = document.createElement('li');
+        recipesListItem.innerHTML =
+`
+<li data-index="${index}">
+    <h3>${recipe.title}</h3>
+    <p><b>Ingredients: </b>${recipe.ingredients}</p>
+    <p><b>Instructions: </b>${recipe.instructions}</p>
+    <button onclick="editRecipe(${index});">Edit</button>
+    <button onclick="deleteRecipe(${index});">Delete</button>
+</li>
+<hr />        
+` 
+    recipesListEl.appendChild(recipesListItem);
+    })
 }
 
-// Edit the recipe object when the Edit button is clicked
+// edit the recipe object when the Edit button is clicked
 function editRecipe(index) {
     // Set a flag to indicate that we are in edit mode
     document.getElementById('add-recipe-btn').setAttribute('data-index', index);
-    // Write your code here for task 4
+    // get selected recipe and display in form
+    const selectedRecipe = recipes[index];
+    console.log(selectedRecipe);
+    document.getElementById('title').value = selectedRecipe.title;
+    document.getElementById('ingredients').value = selectedRecipe.ingredients;
+    document.getElementById('instructions').value = selectedRecipe.instructions;
 }
 
 // Delete the recipe object when the Delete button is clicked
